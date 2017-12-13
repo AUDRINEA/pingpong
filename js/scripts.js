7d12cc02548bf8527ca0ba5fb084a1105d0f51b5
AUDRINEA <audrineamanda@gmail.com>
2017-12-13 16:55:13 +0300

    my pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
-$("form.ping").submit(function (event) {
-   $("#output ul").empty();
-   var number = parseInt($("input").val());
-   for (var index = 1; index <= number; index += 1) {
-       if (index % 15 == 0) {
-           $("#output ul").append('<li>' + "PingPong" + '</li>');
-       }
-       else if (index % 5 == 0) {
-           $("#output ul").append('<li>' + "Pong" + '</li>');
-       }
-        else if (index % 3 == 0) {
-           $("#output ul").append('<li>' + "Ping" + '</li>');
-       }
-        else {
-           $("#output ul").append('<li>' + index + '</li>');
-       }
-   }
-   $("form.ping")[0].reset();
+  var pingPonged = [];
+  //backend
+  function game(number, pingPonged) {
+    for (var index = 1; index <= number; index++) {
+      if (index % 15 === 0) {
+        pingPonged.push("ping pong");
+      } else if (index % 5 === 0) {
+        pingPonged.push("pong");
+      } else if (index % 3 === 0) {
+        pingPonged.push("ping");
+      } else {
+        pingPonged.push(index);
+      }
+    }
+  }
+  //frontend
+  $(document).ready(function() {
+    $("#btn").click(function(event) {
+      event.preventDefault();
+      //$("#output ul").empty();
+      var number = parseInt($("input#pong").val());
+      var ping = game(number, pingPonged);
+      pingPonged.forEach(function(ping) {
+        $("#output1").append("<li>" + ping + "</li>");
+      });
 
-   event.preventDefault();
-});
+      //$("form.ping")[0].reset();
+    });
+  });
